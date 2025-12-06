a3fd6c3619972ab47ee09dabde7ab78d464be04e
Antoine Reversat <a.reversat@gmail.com>
2025-12-06 13:08:26 -0500

    Day03 part 2

--- day03/src/bin/part2.rs
+++ day03/src/bin/part2.rs
@@ -1,10 +1,13 @@
+use anyhow::{bail, Context, Result};
 use std::env;
 use std::fs;
-use anyhow::{Result, bail, Context};
 
-fn read_input(filename: &str) -> Result<Vec<String>> {
+fn read_input(filename: &str) -> Result<Vec<Vec<u32>>> {
     let content = fs::read_to_string(filename)?;
-    Ok(content.lines().map(|l| l.trim().to_string()).collect())
+    Ok(content
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .collect())
 }
 
 fn main() -> Result<()> {
@@ -14,6 +17,33 @@ fn main() -> Result<()> {
     }
 
     let contents = read_input(&args[1])?;
+    let mut powersum = 0;
+
+    for bank in contents {
+        let mut digits: [u32; 12] = [0; 12];
+        let mut next_idx = 0;
+
+        for digit_idx in 0..12 {
+            let mut bank_iter = bank.iter();
+            let offset = next_idx;
+            for (idx, digit) in bank[next_idx..bank.len()-11+digit_idx].iter().enumerate() {
+                if digit  > &digits[digit_idx] {
+                    digits[digit_idx] = *digit;
+                    next_idx = offset + idx + 1;
+                }
+            }
+        }
+
+        println!("{digits:?}");
+        let mut power = String::new();
+        for digit in digits {
+            power.push_str(digit.to_string().as_str());
+        }
+        println!("{power}");
+        powersum += power.parse::<u128>()?;
+    }
+
+    println!("Total power : {powersum}");
 
     Ok(())
 }
